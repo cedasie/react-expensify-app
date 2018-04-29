09668843f7c75ce977ea4d205fe6feb3d801cebd
conquete.asie@gmail.com <conquete.asie@gmail.com>
2018-04-29 17:16:13 +0200

    Add Expensify styles

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoadingPAge = () => (
+    <div className="loader">
+        <img className="loader__image" src="/images/loader.gif" />
+    </div>
+);
+
+export default LoadingPAge;
